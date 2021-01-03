4a1e2f9f9570a06b862b36bd9ad2766899673254
Mateo803 <55462135+Mateo803@users.noreply.github.com>
2021-01-03 18:12:47 +0100

    Add files via upload

---
+++ mvc/control/signUpLoadServlet.java
@@ -0,0 +1,47 @@
+package mvc.control;
+
+
+
+import es.uco.pw.data.dao.Intereses.InteresesDAO;
+
+
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.*;
+
+
+@WebServlet(name="signUpLoad", urlPatterns="/signUpLoad")
+
+
+public class signUpLoadServlet extends HttpServlet{
+
+ protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+
+{
+
+response.setContentType("text/html;charset=UTF-8");
+
+
+String dataBasePath=request.getServletContext().getInitParameter("AbsolutePath")+request.getServletContext().getInitParameter("sqlProperties");
+InteresesDAO interesesDAO = new InteresesDAO(dataBasePath);
+Hashtable<Integer, String> intereses_totales= interesesDAO.DevolverIntereses();
+Enumeration elementos = intereses_totales.elements();
+Enumeration claves = intereses_totales.keys();
+request.setAttribute("elementos", elementos);
+request.setAttribute("claves", claves);
+
+RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("../view/signUpView.jsp");
+
+dispatcher.forward(request, response);
+
+
+
+}
+
+
+}
